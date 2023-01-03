c01cc6eed903032948898550057ac262c0a4ff22
Tanjung Anggraini <you@example.comkareen.anna2@gmail.com>
2023-01-03 12:59:47 +0700

    write fs file using javascript

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const fs = require("fs");
-fs.readFile("text.txt", "utf-8", (err, data) => {
+// fs.readFile("text.txt", "utf-8", (err, data) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+//   console.log(data);
+// });
+
+const content = "i m happy to learned javascript";
+fs.writeFile("text.txt", content, (err) => {
   if (err) {
-    console.error(err);
+    console.err;
     return;
   }
-  console.log(data);
 });
