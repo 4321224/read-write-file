52c9aed3bb62ce1d77bdd587dd276a7c738da4a3
Tanjung Anggraini <you@example.comkareen.anna2@gmail.com>
2023-01-03 13:02:32 +0700

    read and write file in nodejs

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const fs = require("fs");
-// fs.readFile("text.txt", "utf-8", (err, data) => {
-//   if (err) {
-//     console.error(err);
-//     return;
-//   }
-//   console.log(data);
-// });
+fs.readFile("text.txt", "utf-8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
 
 const content = "i m happy to learned javascript";
 fs.writeFile("text.txt", content, (err) => {
